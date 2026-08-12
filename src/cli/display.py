2da4db4fef5e4314f280185dd763890e15a68d9e
BD1224 <benjaminduboshinsky@gmail.com>
2026-08-11 23:19:51 -0700

    fix: fix input buffer bug (when you type, and other thread outputs)

--- src/cli/display.py
+++ src/cli/display.py
@@ -1,8 +1,8 @@
 import shlex  # used to handle inputs with quotes
 from .handler import handle_input
 
-def display(lock):
-    # Note: this uses implicit string conc
+def display(recent_print_flag, lock):
+
     print(
         "************************************************\n"
         "Welcome to internshipper! Type help for commands\n"
@@ -12,10 +12,15 @@ def display(lock):
     while True:
         try:  # shlex may throw an error if there is an unclosed quote
             user_input = shlex.split(input(">> ").lower())  # user_input is a list of inputs
-            with lock: # needed so it doesnt collide with other thread
-                output = handle_input(user_input)
-                if output == 1:  # user typed close
-                    return 0
+            if recent_print_flag.is_set():
+                user_input = ""  # not necessary but just in case
+                print()
+                recent_print_flag.clear()  # resets flag
+            else:
+                with lock: # needed so it doesnt collide with other thread
+                    output = handle_input(user_input)
+                    if output == 1:  # user typed close
+                        return 0
         except ValueError:  # used ValueError to avoid ^C bug, where it doesnt exit
             continue
         except EOFError:
--- src/main.py
+++ src/main.py
@@ -4,28 +4,30 @@ from config import SCRAPING_FREQUENCY
 import threading
 import readline  # even though no code is written with this, it still controls input(), so keys like ^R dont print or interfere
 
-def run_status(flag, lock):  # flag is pass by reference
+def run_status(stop_flag, recent_print_flag, lock):  # flag is pass by reference
     wait_time = 3  # SCRAPING_FREQUENCY*60*60
-    flag.wait(wait_time)  # like sleep, but gets woken when flag is changed
+    stop_flag.wait(wait_time)  # like sleep, but gets woken when flag is changed
 
-    while not flag.is_set():  # is_set return if the flag is True/False
+    while not stop_flag.is_set():  # is_set return if the flag is True/False
         with lock:  # handles .aquire() and .release() and errors
+            recent_print_flag.set()  # tells other thread there was a recent print
             print()
             provide_status()
-            print(">> ", end="", flush=True)  # prints this after the status, because the cli doesnt reprint
-        flag.wait(wait_time)
+            print("Press enter to continue ", end="", flush=True)  # prints this after the status, to avoid saved input buffer
+        stop_flag.wait(wait_time)
     return 0
 
 def main():
-    flag = threading.Event()  # communication between threads (default value is False)
+    stop_flag = threading.Event()  # communication between threads (default value is False)
+    recent_print_flag = threading.Event()
     lock = threading.Lock()  # used to avoid clashes when outputting
 
-    scrape_thread = threading.Thread(target=run_status, args=(flag, lock))  # define thread
+    scrape_thread = threading.Thread(target=run_status, args=(stop_flag, recent_print_flag, lock))  # define thread
     scrape_thread.start()  # run thread
 
-    display(lock) # on main thread run cli display. Will run until user types 'close'
+    display(recent_print_flag, lock) # on main thread run cli display. Will run until user types 'close'
 
-    flag.set()  # sets flag to True/tells thread to stop
+    stop_flag.set()  # sets flag to True/tells thread to stop
     scrape_thread.join()  # waits for thread to return
 
     return 0
